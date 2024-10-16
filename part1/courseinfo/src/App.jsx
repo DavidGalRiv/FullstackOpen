963936d9259cd86fb09d2850067ff9140fd5bff4
DavidGalRiv <davidgalvanr2@gmail.com>
2024-10-16 20:08:59 +0200

    part 1b done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
 
 const Part = (props) => {
   return(
-    <p>{props.part.name} {props.part.exercises} exercises</p>
+    <p>{props.part.name}: {props.part.exercises} exercises</p>
   )
 }
 
