3d404113ecc724ac490cb81078fb879bf7d21911
DavidGalRiv <davidgalvanr2@gmail.com>
2025-03-21 12:19:54 +0100

    exer 1.6

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,58 +1,27 @@
-const Header = (props) => {
-  return(
-  <h1>{props.course}</h1>
+import { useState } from 'react'
+
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
 )
-}
-
-const Part = (props) => {
-  return(
-    <p>{props.part.name}: {props.part.exercises} exercises</p>
-  )
-}
-
-const Content = (props) => {
-  return(
-    <div>
-      <Part part = {props.parts[0]}  />
-      <Part part = {props.parts[1]}  />
-      <Part part = {props.parts[2]}  />
-    </div>
-  )
-}
-
-
-
-const Total = (props) => {
-  return(
-    <p>Total number of exercises: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  )
-}
-
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [value, setValue] = useState(10)
+  
+  const setToValue = newValue => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
   }
 
   return (
     <div>
-      <Header course = {course.name}/>
-      <Content parts = {course.parts}/>
-      <Total parts= {course.parts}/>
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
