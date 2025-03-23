b7c5b2f6665d2a90157feed97a5b03af6ae1a2ea
DavidGalRiv <davidgalvanr2@gmail.com>
2025-03-23 19:59:26 +0100

    exer 2.2

--- part1/courseinfo/src/components/Course.jsx
+++ part1/courseinfo/src/components/Course.jsx
@@ -20,13 +20,25 @@ const Content = ({parts}) => {
     )
 }
 
+const Total = ({parts}) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+    return(
+        <p>
+            <strong>Total of {total} exercises</strong>
+        </p>
+    )
+
+}
+
 const Course = ({course}) => {
     return(
         <div>
             <Header name = {course.name}/>
-            <Content parts ={course.parts}/>
+            <Content parts = {course.parts}/>
+            <Total parts = {course.parts}/>
         </div>
     )
 }
+
 
 export default Course
