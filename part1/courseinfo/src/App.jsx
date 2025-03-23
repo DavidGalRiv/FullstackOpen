3704b9b34364603f72a2963f98800a4db6b41503
DavidGalRiv <davidgalvanr2@gmail.com>
2025-03-23 15:21:00 +0100

    exer 2.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,29 +1,29 @@
-import { useState } from 'react'
-
-const Display = props => <div>{props.value}</div>
-
-const Button = (props) => (
-  <button onClick={props.onClick}>
-    {props.text}
-  </button>
-)
+import Course from "./components/Course"
 
 const App = () => {
-  const [value, setValue] = useState(10)
-  
-  const setToValue = newValue => {
-    console.log('value now', newValue)  // print the new value to console
-    setValue(newValue)
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
 
-  return (
-    <div>
-      <Display value={value} />
-      <Button onClick={() => setToValue(1000)} text="thousand" />
-      <Button onClick={() => setToValue(0)} text="reset" />
-      <Button onClick={() => setToValue(value + 1)} text="increment" />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
