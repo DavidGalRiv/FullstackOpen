73fc63a696f689fa62e29606f509e925f9455fc3
DavidGalRiv <davidgalvanr2@gmail.com>
2025-03-25 14:27:58 +0100

    exer 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -7,15 +7,21 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newName, setNewName] = useState('')
   const [newNumber,setNewNumber] = useState('')
-
+  const [search, setSearch] = useState('')
+  
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -29,12 +35,19 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
-
-
+  
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with: <input value = {search} onChange = {handleSearchChange} />
+      </div>
+
+      <h3> Add a new contact </h3>
       <form onSubmit = {addPerson}>
         <div>
           name: <input value = {newName} onChange = {handleNameChange}/>
@@ -43,12 +56,13 @@ const App = () => {
           number: <input value = {newNumber} onChange = {handleNumberChange}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit"> add </button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      
+      <h3>Numbers</h3>
       <ul>
-        {persons.map((person, index) => (
+        {filteredPersons.map((person, index) => (
           <li key = {index}>{person.name} {person.number}</li>
         ))}
       </ul>
