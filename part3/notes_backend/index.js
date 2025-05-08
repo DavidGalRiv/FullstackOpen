2ebae958723d45e7652c74fd4020be261c20a55b
DavidGalRiv <davidgalvanr2@gmail.com>
2025-05-08 20:49:52 +0200

    part3-4 notes backend

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -1,28 +1,10 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
+
 const app = express()
-const cors = require('cors')
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static('dist'))
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+let notes = []
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -33,83 +15,48 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
+app.use(express.static('dist'))
+app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
+  Note.find({}).then((notes) => {
     response.json(notes)
+  })
 })
-
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then((savedNote) => {
+    response.json(savedNote)
+  })
 })
 
-app.put('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
-  console.log('PUT request received to /api/notes/:id', id)
-  const body = request.body
+  notes = notes.filter((note) => note.id !== id)
 
-  const noteIndex = notes.findIndex(n => n.id === id)
-  if (noteIndex === -1) {
-    return response.status(404).json({ error: 'note not found' })
-  }
-
-  const updatedNote = {
-    ...notes[noteIndex],
-    content: body.content,
-    important: body.important
-  }
-
-  notes[noteIndex] = updatedNote
-
-  response.json(updatedNote)
+  response.status(204).end()
 })
-
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
--- part3/notes_backend/mongo.js
+++ part3/notes_backend/mongo.js
@@ -7,10 +7,9 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://davidgr:${password}@cluster0.vbakrtx.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery',false)
-
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
@@ -25,13 +24,13 @@ const Note = mongoose.model('Note', noteSchema)
 //   important: true,
 // })
 
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
+// // note.save().then((result) => {
+// //   console.log('note saved!')
+// //   mongoose.connection.close()
+// // })
 
-Note.find({}).then(result => {
-  result.forEach(note => {
+Note.find({}).then((result) => {
+  result.forEach((note) => {
     console.log(note)
   })
   mongoose.connection.close()
