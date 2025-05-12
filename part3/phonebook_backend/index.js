ed1b8f79ed1a61a92763d9883c5037b1bd8202a3
DavidGalRiv <davidgalvanr2@gmail.com>
2025-05-13 00:56:18 +0200

    exer 3,15

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -11,11 +11,21 @@ const requestLogger = (request, response, next) => {
     console.log('Body:  ', request.body)
     console.log('---')
     next()
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
   }
   
-app.use(requestLogger)
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
 
 morgan.token("body", (req) => {
     return req.method === "POST" ? JSON.stringify(req.body) : ""
@@ -23,7 +33,6 @@ morgan.token("body", (req) => {
   
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
-let persons= []
 
 app.get("/api/persons", (request, response) =>{
     Person.find({}).then((persons) => {
@@ -31,15 +40,16 @@ app.get("/api/persons", (request, response) =>{
     })
 })
 
-app.get("/api/persons/:id", (request, response) =>{
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    
-    if(person){
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get("/api/persons/:id", (request, response, next) =>{
+    Person.findById(request.params.id)
+        .then(person => {
+          if (person) {
+            response.json(person)
+          } else {
+            response.status(404).end()
+          }
+        })
+        .catch(error => next(error))
 })
 
 
@@ -54,42 +64,53 @@ app.get("/info", (request, response) => {
    
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+// const generateId = () => {
+//     const maxId = persons.length > 0
+//         ? Math.max(...persons.map(n => Number(n.id)))
+//         : 0
+//     return String(maxId + 1)
+// }
 
-    response.status(204).end()
+// app.post("/api/persons", (request, response) => {
+//     const {name, number} = request.body
+//     console.log("Request body:", request.body)
+//     if(!name || !number){
+//         return response.status(404).json({error: "name or number is missing"})
+//     }
+
+//     const nameExists = persons.some(person => person.name === name)
+//     if(nameExists){
+//         return response.status(404).json({error: "name must be unique"})
+//     }
+
+//     const person = new Person({
+//         id: generateId(),
+//         name,
+//         number,
+//     })
+
+//     person.save().then((savedPerson) =>{
+//         response.json(savedPerson)
+//     })
+// })
+
+app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
 })
 
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
-}
 
-app.post("/api/persons", (request, response) => {
-    const {name, number} = request.body
-    console.log("Request body:", request.body)
-    if(!name || !number){
-        return response.status(404).json({error: "name or number is missing"})
-    }
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-    const nameExists = persons.some(person => person.name === name)
-    if(nameExists){
-        return response.status(404).json({error: "name must be unique"})
-    }
-
-    const person = new Person({
-        id: generateId(),
-        name,
-        number,
-    })
-
-    person.save().then((savedPerson) =>{
-        response.json(savedPerson)
-    })
-})
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
