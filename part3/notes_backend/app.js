860dff0ca91d6bc8e466c8ef9144674285401ca4
DavidGalRiv <davidgalvanr2@gmail.com>
2025-05-16 13:26:02 +0200

    requestlogger now working, placed incorrectly within .js

--- part3/notes_backend/app.js
+++ part3/notes_backend/app.js
@@ -21,10 +21,11 @@ app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 
+app.use(middleware.requestLogger)
+
 app.use('/api/notes', notesRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
-app.use(middleware.requestLogger)
 
 module.exports = app
