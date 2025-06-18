8f8a86ca0e2cac701edca647a62552386d0442df
DavidGalRiv <davidgalvanr2@gmail.com>
2025-06-18 19:39:51 +0200

    part4-8

--- part3/notes_backend/app.js
+++ part3/notes_backend/app.js
@@ -1,30 +1,31 @@
 require('express-async-errors')
 const express = require('express')
-const app = express()
-const cors = require('cors')
 const mongoose = require('mongoose')
-const notesRouter = require('./controllers/notes')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
+const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
 
+const app = express()
+
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('Connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
-  .catch((error) => {
-    logger.error('Error connecting to MongoDB:', error.message)
+  .catch(error => {
+    logger.error('error connection to MongoDB:', error.message)
   })
 
-app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
-
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
