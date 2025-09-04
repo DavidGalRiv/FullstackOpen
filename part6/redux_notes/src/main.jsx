64c43849b036327383d277a2b0be9f0ad42515b7
DavidGalRiv <davidgalvanr2@gmail.com>
2025-09-04 12:56:37 +0200

    part 6-4

--- part6/redux_notes/src/main.jsx
+++ part6/redux_notes/src/main.jsx
@@ -5,8 +5,10 @@ import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
-import noteReducer from './reducers/noteReducer'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
+
+import noteService from './services/notes'
 
 const store = configureStore({
   reducer: {
@@ -14,6 +16,10 @@ const store = configureStore({
     filter: filterReducer
   }
 })
+
+noteService.getAll().then(notes =>
+    store.dispatch(setNotes(notes))
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- part6/redux_notes/src/reducers/noteReducer.js
+++ part6/redux_notes/src/reducers/noteReducer.js
@@ -42,9 +42,15 @@ const noteSlice = createSlice({
       return state.map(note =>
         note.id !== id ? note : changedNote 
       )     
+    },
+    appendNote(state, action) {
+      state.push(action.payload)
+    },
+    setNotes(state, action) {
+      return (action.payload)
     }
   },
 })
 
-export const {createNote,toggleImportanceOf} = noteSlice.actions
+export const {createNote,toggleImportanceOf, appendNote, setNotes} = noteSlice.actions
 export default noteSlice.reducer
